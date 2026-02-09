4da181f60595da7b86b1810f2c5498438788e8c3
VueMid <turabovumid587@gmail.com>
2026-02-09 10:34:21 +0500

    Continue...(02.09.2026/10:35)

--- main.js
+++ main.js
@@ -1,35 +1,27 @@
-let body = document.querySelector("body");
-let navbar = document.querySelector(".header__mb-nav");
+let div = document.querySelector(".header__mb-div");
 let openBurger = document.querySelector(".header__open-burger");
 let closeBurger = document.querySelector(".header__close-burger");
-let div = document.querySelector(".header__mb-div");
 let links = document.querySelectorAll(".header__mb-links-texts");
+let button = document.querySelector(".header__mb-button");
 openBurger.addEventListener("click", () => {
-  navbar.style.display = "flex";
   closeBurger.style.display = "block";
   openBurger.style.display = "none";
   div.style.opacity = "1";
-  navbar.style.transition = "500ms ease all !important";
-  div.style.transition = "500ms ease all !important";
-  body.style.overflow = "hidden";
 });
 closeBurger.addEventListener("click", () => {
-  navbar.style.display = "none";
   closeBurger.style.display = "none";
   openBurger.style.display = "block";
   div.style.opacity = "0";
-  navbar.style.transition = "500ms ease all !important";
-  div.style.transition = "500ms ease all !important";
-  body.style.overflow = "auto";
 });
 links.forEach((link) => {
   link.addEventListener("click", () => {
-    navbar.style.display = "none";
     closeBurger.style.display = "none";
     openBurger.style.display = "block";
     div.style.opacity = "0";
-    navbar.style.transition = "500ms ease all !important";
-    div.style.transition = "500ms ease all !important";
-    body.style.overflow = "auto";
   });
+});
+button.addEventListener("click", () => {
+  closeBurger.style.display = "none";
+  openBurger.style.display = "block";
+  div.style.opacity = "0";
 });
