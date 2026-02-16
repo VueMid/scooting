1638bb5d2a0330b6efedb4d926561e63f33bc1e2
VueMid <turabovumid587@gmail.com>
2026-02-16 10:44:11 +0500

    Continue...(02.16.2026/10:45)

--- main.js
+++ main.js
@@ -1,27 +1,24 @@
-let div = document.querySelector(".header__mb-div");
-let openburger = document.querySelector(".header__open-burger");
-let closeburger = document.querySelector(".header__close-burger");
+let openBurger = document.querySelector(".header__open-burger");
+let closeBurger = document.querySelector(".header__close-burger");
+let mainBurger = document.querySelector(".header__mb-buttons");
+let mainDiv = document.querySelector(".header__mb-div");
+let navButton = document.querySelector(".header__mb-button");
 let links = document.querySelectorAll(".header__mb-links-texts");
-let button = document.querySelector(".header__mb-button");
-openburger.addEventListener("click", () => {
-  div.style.opacity = "1";
-  closeburger.style.display = "block";
-  openburger.style.display = "none";
+function closeMenu() {
+  mainDiv.classList.remove("active");
+  openBurger.style.display = "block";
+  closeBurger.style.display = "none";
+}
+mainBurger.addEventListener("click", () => {
+  mainDiv.classList.toggle("active");
+  if (mainDiv.classList.contains("active")) {
+    openBurger.style.display = "none";
+    closeBurger.style.display = "block";
+  } else {
+    closeMenu();
+  }
 });
-closeburger.addEventListener("click", () => {
-  div.style.opacity = "0";
-  closeburger.style.display = "none";
-  openburger.style.display = "block";
-});
+navButton.addEventListener("click", closeMenu);
 links.forEach((link) => {
-  link.addEventListener("click", () => {
-    div.style.opacity = "0";
-    closeburger.style.display = "none";
-    openburger.style.display = "block";
-  });
-});
-button.addEventListener("click", () => {
-  div.style.opacity = "0";
-  closeburger.style.display = "none";
-  openburger.style.display = "block";
+  link.addEventListener("click", closeMenu);
 });
